6dc22c6696283d28c070e77a03ab9fd877c0ff26
Sergei <maslennikovserge@yandex.ru>
2022-10-07 22:57:48 +0300

    mxupdate: LIB.Share

--- Update/Dev/devDataSetConfig.h
+++ Update/Dev/devDataSetConfig.h
@@ -2,6 +2,8 @@
 
 #include <utilsBase.h>
 
+#include <shareConfig.h>
+
 #include <string>
 
 #include <boost/property_tree/ptree.hpp>
@@ -12,15 +14,6 @@ namespace dev
 namespace config
 {
 
-struct tDevice
-{
-	std::string Type;
-	utils::tVersion Version;
-
-	tDevice() = default;
-	explicit tDevice(boost::property_tree::ptree pTree);
-};
-
 struct tUpdateServer
 {
 	std::string Host;
@@ -28,21 +21,21 @@ struct tUpdateServer
 	std::string TargetList;
 
 	tUpdateServer() = default;
-	explicit tUpdateServer(boost::property_tree::ptree pTree);
+	explicit tUpdateServer(const boost::property_tree::ptree& pTree);
 };
 
 }
 
 class tDataSetConfig
 {
-	config::tDevice m_Device;
+	share_config::tDevice m_Device;
 	config::tUpdateServer m_UpdateServer;
 	std::string m_UpdatePath;
 
 public:
 	tDataSetConfig(const std::string& fileNameConfig, const std::string& fileNameDevice);
 
-	config::tDevice GetDevice() const { return m_Device; }
+	share_config::tDevice GetDevice() const { return m_Device; }
 	config::tUpdateServer GetUpdateServer() const { return m_UpdateServer; }
 	std::string GetUpdatePath() const { return m_UpdatePath; }
 };
