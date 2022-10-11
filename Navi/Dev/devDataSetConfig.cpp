326b313cc210196b455af457b0f3409f0196f790
Sergei <maslennikovserge@yandex.ru>
2022-10-11 22:41:34 +0300

    mxnavi: LIB.Share

--- Navi/Dev/devDataSetConfig.cpp
+++ Navi/Dev/devDataSetConfig.cpp
@@ -10,13 +10,7 @@ namespace dev
 namespace config
 {
 
-tDevice::tDevice(boost::property_tree::ptree pTree)
-{
-	Type = pTree.get<std::string>("device.type");
-	Version = utils::tVersion(pTree.get<std::string>("firmware.version"));
-}
-
-tEmail::tEmail(boost::property_tree::ptree pTree)
+tEmail::tEmail(const boost::property_tree::ptree& pTree)
 {
 	To = pTree.get<std::string>("mxnavi.email.to");
 	Period = pTree.get<uint32_t>("mxnavi.email.period");
@@ -25,32 +19,6 @@ tEmail::tEmail(boost::property_tree::ptree pTree)
 bool tEmail::IsWrong() const
 {
 	return To.empty() || Period == 0; //[TBD] check if email is correct
-}
-
-tGNSS::tGNSS(boost::property_tree::ptree pTree)
-{
-	std::string PathRaw = pTree.get<std::string>("gnss.path");
-	Path = utils::linux::GetPath(PathRaw);
-	Prefix = pTree.get<std::string>("gnss.prefix");
-	QtyMax = pTree.get<uint8_t>("gnss.qtyMax");
-}
-
-bool tGNSS::IsWrong() const
-{
-	return Path.empty() || Prefix.empty() || QtyMax == 0;;
-}
-
-tPicture::tPicture(boost::property_tree::ptree pTree)
-{
-	std::string PathRaw = pTree.get<std::string>("picture.path");
-	Path = utils::linux::GetPath(PathRaw);
-	Prefix = pTree.get<std::string>("picture.prefix");
-	QtyMax = pTree.get<uint8_t>("picture.qtyMax");
-}
-
-bool tPicture::IsWrong() const
-{
-	return Path.empty() || Prefix.empty() || QtyMax == 0;
 }
 
 }
@@ -64,7 +32,7 @@ tDataSetConfig::tDataSetConfig(const std::string& fileNameConfig, const std::str
 
 	boost::property_tree::ptree PTreeDevice;
 	boost::property_tree::json_parser::read_json(fileNameDevice, PTreeDevice);
-	m_Device = config::tDevice(PTreeDevice);
+	m_Device = share_config::tDevice(PTreeDevice);
 
 	boost::property_tree::ptree PTreePrivate;
 	boost::property_tree::json_parser::read_json(fileNamePrivate, PTreePrivate);
