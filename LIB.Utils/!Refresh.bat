ed47267380609c537a950b2c2131e5f0bb3e6fbc
Sergei <maslennikovserge@yandex.ru>
2022-11-07 21:14:30 +0300

    LIB.Utils: add. packet NMEA, log, shell, etc.

--- LIB.Utils/!Refresh.bat
+++ LIB.Utils/!Refresh.bat
@@ -3,8 +3,17 @@ del /Q *.h
 
 xcopy /Y %LIB_UTILS%\utilsBase.*
 xcopy /Y %LIB_UTILS%\utilsChrono.*
+xcopy /Y %LIB_UTILS%\utilsCRC.*
 xcopy /Y %LIB_UTILS%\utilsLinux.*
+xcopy /Y %LIB_UTILS%\utilsLog.*
+xcopy /Y %LIB_UTILS%\utilsPacket.*
+xcopy /Y %LIB_UTILS%\utilsPacketNMEA.*
+xcopy /Y %LIB_UTILS%\utilsPacketNMEAPayload.*
+xcopy /Y %LIB_UTILS%\utilsPacketNMEAPayloadPTWS.*
+xcopy /Y %LIB_UTILS%\utilsPacketNMEAType.*
 xcopy /Y %LIB_UTILS%\utilsPath.*
+xcopy /Y %LIB_UTILS%\utilsSerialPort.*
+xcopy /Y %LIB_UTILS%\utilsShell.*
 xcopy /Y %LIB_UTILS%\utilsWeb.*
 
 set ARCH_7z2201="7z2201"
