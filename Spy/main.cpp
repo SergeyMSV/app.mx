28eaa24f7f50cc0b6a9b35f8c8b5663045039998
Sergei <maslennikovserge@yandex.ru>
2022-11-15 20:19:56 +0300

    mxspy: it repeats request if the previous one failed

--- Spy/main.cpp
+++ Spy/main.cpp
@@ -22,17 +22,17 @@
 std::string ParseGLO(const boost::property_tree::ptree& pTree);
 std::string ParseGPS(const boost::property_tree::ptree& pTree);
 
-void GetGNSSState(const std::string& host, const std::string& target, const share_config::tOutFile& cfgOut, std::function<std::string(const boost::property_tree::ptree&)> handler)
+bool GetGNSSState(const std::string& host, const std::string& target, const share_config::tOutFile& cfgOut, std::function<std::string(const boost::property_tree::ptree&)> handler)
 {
 #if defined(MXSPY_TEST)
 	//std::fstream DataRaw = std::fstream("DataRaw_gps.txt", std::ios::in);
 	std::fstream DataRaw = std::fstream("DataRaw_glonass.txt", std::ios::in);
 	if (!DataRaw.is_open())
-		return;//[TBD] throw an exception
+		return false;
 #else
 	std::optional<std::string> DataRawOpt = utils::web::HttpsReqSyncString(host, target);
 	if (!DataRawOpt.has_value())
-		return;//[TBD] throw an exception
+		return false;
 
 	std::stringstream DataRaw(DataRawOpt.value());
 #endif
@@ -44,7 +44,7 @@ void GetGNSSState(const std::string& host, const std::string& target, const shar
 	std::string FileNameTemp = Path + g_FileNameTempPrefix + FileName + ".tmp";
 	std::fstream File = std::fstream(FileNameTemp, std::ios::out);
 	if (!File.good())
-		return;//[TBD] throw an exception
+		return true;//true means - do not repeate [TBD] throw an exception
 
 	std::string DataJSON;
 	try
@@ -87,6 +87,8 @@ void GetGNSSState(const std::string& host, const std::string& target, const shar
 #if defined(MXSPY_TEST)
 	DataRaw.close();
 #endif
+
+	return true;
 }
 
 int main(int argc, char* argv[])
@@ -101,27 +103,31 @@ int main(int argc, char* argv[])
 		std::string PathFileConfig = utils::linux::GetPathConfigExc(PathFile.string());
 		std::string PathFileDevice = utils::linux::GetPathConfigExc("mxdevice");
 
-		utils::tTimePeriod TimePeriod(true);
+		utils::tTimePeriodCount TimePeriod(true);
 
 		while (true)
 		{
 			dev::tDataSetConfig DsConfig(PathFileConfig, PathFileDevice);
-
 			dev::config::tSpyGNSS SpyGNSS = DsConfig.GetSpyGNSS();
-
 			if (SpyGNSS.IsWrong())
 				return static_cast<int>(utils::tExitCode::EX_CONFIG);
 
-			TimePeriod.Set(SpyGNSS.Period);
+			if (TimePeriod.GetPeriod() != SpyGNSS.Period || TimePeriod.GetRepPeriod() != SpyGNSS.RepPeriod || TimePeriod.GetRepQty() != SpyGNSS.RepQty)
+				TimePeriod.Set(SpyGNSS.Period, SpyGNSS.RepPeriod, SpyGNSS.RepQty, false);
 
 			if (!TimePeriod.IsReady())
 			{
-				std::this_thread::sleep_for(std::chrono::milliseconds(60000));
+				std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 				continue;
 			}
 
-			GetGNSSState(SpyGNSS.Host, SpyGNSS.TargetGLO, DsConfig.GetSpyOutGLO(), ParseGLO);
-			GetGNSSState(SpyGNSS.Host, SpyGNSS.TargetGPS, DsConfig.GetSpyOutGPS(), ParseGPS);
+			if (!GetGNSSState(SpyGNSS.Host, SpyGNSS.TargetGLO, DsConfig.GetSpyOutGLO(), ParseGLO))
+				continue;
+
+			if (!GetGNSSState(SpyGNSS.Host, SpyGNSS.TargetGPS, DsConfig.GetSpyOutGPS(), ParseGPS))
+				continue;
+
+			TimePeriod.Complete();
 		}
 	}
 	catch (std::exception& e)
