2d5ff43f7191896e644074163f1a3d69d5c80c46
Sergei <maslennikovserge@yandex.ru>
2022-11-16 20:06:01 +0300

    mxgnss: some slight modifications

--- LIB.Module/!Refresh.bat
+++ LIB.Module/!Refresh.bat
@@ -4,15 +4,6 @@ del /Q *.h
 set LIB_MODULE_GNSS=%LIB_MODULE%.GNSS
 
 xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnss.*
-xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnssReceiver.*
-xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnssReceiver_State.*
-xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnssReceiver_State_CmdGPI.*
-xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnssReceiver_State_CmdGPO.*
-xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnssReceiver_State_CmdREQ.*
-xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnssReceiver_StateError.*
-xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnssReceiver_StateHalt.*
-xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnssReceiver_StateOperation.*
-xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnssReceiver_StateStart.*
-xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnssReceiver_StateStop.*
+xcopy /Y %LIB_MODULE_GNSS%\LIB.Module\modGnssReceiver*
 
 pause
