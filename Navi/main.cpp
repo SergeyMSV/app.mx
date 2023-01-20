2a3fda91c271ec4b10c0244aeefebf47e493a0d2
Sergei <maslennikovserge@yandex.ru>
2023-01-20 15:43:05 +0300

    mxnavi: log

--- Navi/main.cpp
+++ Navi/main.cpp
@@ -8,22 +8,17 @@
 
 #include <shareConfig.h>
 #include <shareHTML.h>
+#include <shareUtils.h>
+#include <shareUtils.h>
 
 #include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <sstream>
 #include <thread>
 
 //$ echo "Hello this is the body message, we are sending email with attachement using mutt and msmtp" | mutt -a "/home/sergey/cam.jpg" -a "/home/sergey/cam_2.jpg" -s "this is the subject of the message" --satellite4@yandex.ru
 //$ cat ~/email/attached_file.txt | mutt -a "/home/sergey/cam.jpg" -a "/home/sergey/cam_2.jpg" -s "this is the subject of the message" --satellite4@yandex.ru
-
-std::deque<std::string> GetFilePaths(const share_config::tOutFile& conf)
-{
-	utils::RemoveFilesOutdated(conf.Path, conf.Prefix, conf.QtyMax);
-	auto List = utils::GetFilesLatest(conf.Path, conf.Prefix, conf.QtyMax);
-	utils::linux::CorrPaths(List);
-	return List;
-}
 
 int main(int argc, char* argv[])
 {
@@ -50,12 +45,14 @@ int main(int argc, char* argv[])
 			const dev::config::tPicture ConfPict = DsConfig.GetPicture();
 			const dev::config::tSpyOutGLO ConfSpyOutGLO = DsConfig.GetSpyOutGLO();
 			const dev::config::tSpyOutGPS ConfSpyOutGPS = DsConfig.GetSpyOutGPS();
+			const dev::config::tLog ConfLog = DsConfig.GetLog();
 
 			if (ConfEmail.IsWrong() ||
 				ConfGnss.IsWrong() ||
 				ConfPict.IsWrong() ||
 				ConfSpyOutGLO.IsWrong() ||
-				ConfSpyOutGPS.IsWrong())
+				ConfSpyOutGPS.IsWrong() ||
+				ConfLog.IsWrong())
 				return static_cast<int>(utils::tExitCode::EX_CONFIG);
 			
 			if (TimePeriod.GetPeriod() != ConfEmail.Period)
@@ -67,10 +64,15 @@ int main(int argc, char* argv[])
 				continue;
 			}
 
-			auto GnssList = GetFilePaths(ConfGnss);
-			auto PictList = GetFilePaths(ConfPict);
-			auto SpyOutGLOList = GetFilePaths(ConfSpyOutGLO);
-			auto SpyOutGPSList = GetFilePaths(ConfSpyOutGPS);
+			share::tLogFile LogFile(ConfLog);
+			LogFile.Write("Start");
+			
+			auto GnssList = share::GetFilePaths(ConfGnss);
+			auto PictList = share::GetFilePaths(ConfPict);
+			auto SpyOutGLOList = share::GetFilePaths(ConfSpyOutGLO);
+			auto SpyOutGPSList = share::GetFilePaths(ConfSpyOutGPS);
+
+			LogFile.Write("Lists loaded");
 
 			dev::tDataSetGNSS DsGNSS{};
 			try
@@ -88,6 +90,8 @@ int main(int argc, char* argv[])
 			{
 				std::cerr << "[ERR-JSON] " << e.what() << "\n";
 			}
+
+			LogFile.Write("DataSets loaded");
 
 			std::string Cmd;
 			Cmd = "echo ";
@@ -108,22 +112,15 @@ int main(int argc, char* argv[])
 			Cmd += " -s \"" + utils::linux::CmdLine("hostname") + ": Schedule Snapshot Recording\"";
 			Cmd += " -- " + ConfEmail.To;
 
-			int ResCode = 0;
+			LogFile.Write(Cmd);
 
+			int ResCode = 0;
 #if defined(_WIN32)
 			std::cout << Cmd << '\n';
 #else 
 			ResCode = system(Cmd.c_str());
 #endif
-
-			if (ResCode == 0)//[TBD] check it
-			{
-				for (auto& i : PictList)
-				{
-					std::error_code ErrCode;
-					std::filesystem::remove(i, ErrCode);
-				}
-			}
+			LogFile.Write("ResCode: " + std::to_string(ResCode));
 		}
 	}
 	catch (const std::exception& e)
