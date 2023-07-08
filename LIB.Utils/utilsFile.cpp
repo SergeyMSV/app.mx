295e269b986ecd0acb54d46bd5a9ea4019b7f14f
Sergei <maslennikovserge@yandex.ru>
2023-07-08 18:56:02 +0300

    mxnavi: it doesn't delete files created by other applications

--- LIB.Utils/utilsFile.cpp
+++ LIB.Utils/utilsFile.cpp
@@ -7,6 +7,19 @@ namespace utils
 	namespace file
 	{
 
+std::optional<std::string> ReplaceFileNamePrefix(const std::string& path, const std::string& from, const std::string& to)
+{
+	const std::size_t PosSlash = path.find_last_of('/');
+	const bool IsPath = PosSlash != std::string::npos;
+	const size_t Pos = path.find(from, IsPath ? PosSlash : 0);
+
+	if (Pos == std::string::npos || IsPath && Pos != PosSlash + 1)
+		return std::nullopt;
+
+	std::string PathRes = path;
+	return PathRes.replace(Pos, from.length(), to);
+}
+
 std::deque<std::string> GetFilesLatest(const std::string& path, const std::string& prefix, size_t qtyFilesLatest)
 {
 	std::deque<std::string> List;
@@ -44,6 +57,18 @@ void RemoveFilesOutdated(const std::string& path, const std::string& prefix, siz
 	std::deque<std::string> List = GetFilesLatest(path, prefix, 0);
 
 	while (List.size() > qtyFilesLatest)
+	{
+		std::error_code ErrCode;
+		std::filesystem::remove(List.front(), ErrCode);
+		List.pop_front();
+	}
+}
+
+void RemoveFilesWithPrefix(const std::string& path, const std::string& prefix)
+{
+	std::deque<std::string> List = GetFilesLatest(path, prefix, 0);
+
+	while (!List.empty())
 	{
 		std::error_code ErrCode;
 		std::filesystem::remove(List.front(), ErrCode);
--- LIB.Utils/utilsFile.h
+++ LIB.Utils/utilsFile.h
@@ -9,14 +9,18 @@
 
 #include <deque>
 #include <string>
+#include <optional>
 
 namespace utils
 {
 	namespace file
 	{
 
+std::optional<std::string> ReplaceFileNamePrefix(const std::string& path, const std::string& from, const std::string& to);
+
 std::deque<std::string> GetFilesLatest(const std::string& path, const std::string& prefix, size_t qtyFilesLatest);
 void RemoveFilesOutdated(const std::string& path, const std::string& prefix, size_t qtyFilesLatest);
+void RemoveFilesWithPrefix(const std::string& path, const std::string& prefix);
 
 	}
 }
