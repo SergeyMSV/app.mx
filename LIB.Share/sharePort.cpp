45ca782ff861190f79608131a40e51fc1c120f11
Sergei <maslennikovserge@yandex.ru>
2023-11-01 20:37:10 +0300

    TWR: it breaks the receiving process through blocking socket (receive_from) if no data can be received

--- LIB.Share/sharePort.cpp
+++ LIB.Share/sharePort.cpp
@@ -7,7 +7,8 @@ namespace share_port
 {
 
 tTWRClient::tTWRClient(boost::asio::io_context& ioc)
-	:m_Resolver(asio_ip::udp::resolver(ioc)), m_Socket(asio_ip::udp::socket(ioc))
+	:m_Resolver(asio_ip::udp::resolver(ioc)), m_Socket(asio_ip::udp::socket(ioc)),
+	m_State(tState::None), m_CtrlStateThread(&tTWRClient::CtrlStateThread, this)
 {
 	m_ReceiverEndpoint = *m_Resolver.resolve(asio_ip::udp::v4(), Host, std::to_string(MXTWR_PORT)).begin();
 	m_Socket.open(asio_ip::udp::v4());
@@ -15,6 +16,10 @@ tTWRClient::tTWRClient(boost::asio::io_context& ioc)
 
 tTWRClient::~tTWRClient()
 {
+	m_State = tState::Close;
+	m_cv.notify_all();
+	m_CtrlStateThread.join();
+
 	m_Socket.close();
 }
 
@@ -30,12 +35,19 @@ void tTWRClient::Transaction_SPI_SetChipControl(TWR::tEndpoint ep, TWR::tChipCon
 
 tPacketTWRRsp tTWRClient::Transaction(const tPacketTWRCmd& cmd)
 {
+	SetState(tState::Write);
+
 	tVectorUInt8 Pack = cmd.ToVector();
 	m_Socket.send_to(boost::asio::buffer(Pack.data(), Pack.size()), m_ReceiverEndpoint);
+
+	SetState(tState::Read);
 
 	std::array<char, share_network_udp::PacketSizeMax> ReceiveBuffer;
 	asio_ip::udp::endpoint SenderEndpoint;
 	std::size_t Size = m_Socket.receive_from(boost::asio::buffer(ReceiveBuffer), SenderEndpoint);
+
+	SetState(tState::None);
+
 	if (!Size)
 		return {};
 	tVectorUInt8 ReceivedData(ReceiveBuffer.begin(), ReceiveBuffer.begin() + Size);
@@ -44,6 +56,46 @@ tPacketTWRRsp tTWRClient::Transaction(const tPacketTWRCmd& cmd)
 	if (!PackSize)
 		return {};
 	return Rsp;
+}
+
+void tTWRClient::SetState(tState state)
+{
+	m_State = state;
+	m_cv.notify_all();
+}
+
+void tTWRClient::CtrlState()
+{
+	while (m_State != tState::Close)
+	{
+		switch (m_State)
+		{
+		case  tState::None:
+		{
+			std::unique_lock<std::mutex> Lock(m_cv_mtx);
+			m_cv.wait(Lock);
+			break;
+		}
+		case tState::Read:
+		case tState::Write:
+		{
+			std::unique_lock<std::mutex> Lock(m_cv_mtx);
+			if (m_cv.wait_for(Lock, std::chrono::seconds(1)) == std::cv_status::timeout) //[#] < 1 s. That is available period of time to send/receive.
+			{
+				// It breaks the receiving process through blocking socket (receive_from) if no data can be received for any reason.
+				m_Socket.shutdown(boost::asio::socket_base::shutdown_both);
+				m_Socket.close();
+			}
+			break;
+		}
+		}
+	}
+}
+
+void tTWRClient::CtrlStateThread(void* obj)
+{
+	auto TWRClient = static_cast<tTWRClient*>(obj);
+	TWRClient->CtrlState();
 }
 
 }
--- LIB.Share/sharePort.h
+++ LIB.Share/sharePort.h
@@ -3,7 +3,10 @@
 
 #ifdef MXTWR_CLIENT
 
+#include <atomic>
+#include <condition_variable>
 #include <string>
+#include <thread>
 #include <vector>
 
 #include <utilsPacketTWR.h>
@@ -25,17 +28,36 @@ class tTWRClient
 	asio_ip::udp::endpoint m_ReceiverEndpoint;
 	asio_ip::udp::socket m_Socket;
 
+	enum class tState
+	{
+		None,
+		Read,
+		Write,
+		Close,
+	};
+	std::atomic<tState> m_State = tState::None; // it's used by m_CtrlStateThread
+	std::condition_variable m_cv;
+	std::mutex m_cv_mtx;
+	std::thread m_CtrlStateThread;
+
 public:
 	tTWRClient() = delete;
-	tTWRClient(boost::asio::io_context& ioc);
+	explicit tTWRClient(boost::asio::io_context& ioc);
 	tTWRClient(const tTWRClient&) = delete;
+	tTWRClient(tTWRClient&&) = delete;
 	~tTWRClient();
+	tTWRClient& operator=(const tTWRClient&) = delete;
+	tTWRClient& operator=(tTWRClient&&) = delete;
 
 	std::vector<std::uint8_t> Transaction_SPI_Request(TWR::tEndpoint ep, const std::vector<std::uint8_t>& tx);
 	void Transaction_SPI_SetChipControl(TWR::tEndpoint ep, TWR::tChipControl tx);
 
 private:
 	tPacketTWRRsp Transaction(const tPacketTWRCmd& cmd);
+
+	void SetState(tState state);
+	void CtrlState();
+	static void CtrlStateThread(void* obj);
 };
 
 }
