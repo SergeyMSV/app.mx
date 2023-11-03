1dd619acf277e42381f6ea3239a54a30ac1cec94
Sergei <maslennikovserge@yandex.ru>
2023-11-03 21:49:23 +0300

    WebUICard: hex-frame for card UID and Payload; colour status

--- WebUICard/server_card.js
+++ WebUICard/server_card.js
@@ -9,14 +9,19 @@ exports.StartPipeReading = function (a_config) {
 }
 
 exports.GetPage = function () {
-    return '<tr><td></td><td>Card Type:</td><td id="card_type"></td></tr>' +
-        '<tr><td id="card_status" bgcolor="white"></td><td>Card UID:</td><td id="card_uid"></td></tr>';
+    return '<tr><td id="card_status" bgcolor="white"></td><td width="100">Card:</td><td id="card_type"></td></tr>' +
+        '<tr><td></td><td id="card_content" colspan="2">' +
+        '<table>' +
+        '<tr><td>UID:</td><td id="card_uid" class="hex_box"></td></tr>' +
+        '<tr><td>Payload:</td><td id="card_payload" class="hex_box"></td></tr>' +
+        '</table>' +
+        '</td></td></tr>';
 }
 
 exports.GetPageData = function (a_config) {
     let data = {};
     data.card_type = GetCardType(g_ReadPipe);
-    data.card_status = 'yellow';
+    data.card_status = GetCardTypeColour(g_ReadPipe);
     data.card_data = g_ReadPipe;
     return data;
 }
@@ -38,17 +43,23 @@ function ReadPipe(a_pipeName) {
     setTimeout(() => { ReadPipe(a_pipeName) }, 100); // Nyquist sampling theorem 1/2F (?)
 }
 
-function GetCardType(a_card) {
+function GetCardTypeNum(a_card) {
     // http://www.nxp.com/documents/application_note/AN10833.pdf 
     // 3.2 Coding of Select Acknowledge (SAK)
     // ignore 8-bit (iso14443 starts with LSBit = bit 1)
     // fixes wrong type for manufacturer Infineon (http://nfc-tools.org/index.php?title=ISO14443A)
     if (a_card === undefined || typeof (a_card.uid) != 'string' || a_card.uid.length == 0)
-        return '';
+        return -1;
     let sak = Number(a_card.sak);
     sak &= 0x7F;
-    switch (sak) {
-        case 0x04: return 'UID not complete';  // UID not complete
+    return sak;
+}
+
+function GetCardType(a_card) {
+    const cardType = GetCardTypeNum(a_card);
+    switch (cardType) {
+        case -1: return '';
+        case 0x04: return 'UID not complete';
         case 0x09: return 'MIFARE MINI';
         case 0x08: return 'MIFARE 1K';
         case 0x18: return 'MIFARE 4K';
@@ -59,5 +70,24 @@ function GetCardType(a_card) {
         case 0x20: return 'ISO 14443 4';
         case 0x40: return 'ISO 18092';
         default: return 'UNKNOWN';
+    }
+}
+
+function GetCardTypeColour(a_card) {
+    const cardType = GetCardTypeNum(a_card);
+    switch (cardType) {
+        case -1: return 'gray';
+        case 0x04: return 'red'; // UID not complete
+        case 0x09: // MIFARE MINI
+        case 0x08: // MIFARE 1K
+        case 0x18: return 'yellow'; // MIFARE 4K
+
+        case 0x00: return 'green'; // MIFARE UL
+        case 0x10:
+        case 0x11: // MIFARE PLUS
+        case 0x01: // TNP3XXX
+        case 0x20: // ISO 14443 4
+        case 0x40: return 'yellow'; // ISO 18092;
+        default: return 'orange';
     }
 }
