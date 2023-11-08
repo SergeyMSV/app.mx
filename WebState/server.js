cbcc9ab3357cb75fd2bd3a4c2397efb9f6e79de7
Sergei <maslennikovserge@yandex.ru>
2023-11-08 23:23:11 +0300

    WebState: the page plays special sound in case if no response received

--- WebState/server.js
+++ WebState/server.js
@@ -121,4 +121,10 @@ app.get([
         });
     });
 
+app.get('/sound/no_answer_peep.mp3', (req, res) => {
+    np_fs.readFile(__dirname + '/public' + req.url, (err, file) => {
+        res.setHeader('Content-Type', 'audio/mpeg'); res.end(file);
+    });
+});
+
 app.listen(g_port);
