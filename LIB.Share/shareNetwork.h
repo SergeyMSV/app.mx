f5777e7e3d6207baf3348abcf9f6911583cf421c
Sergei <maslennikovserge@yandex.ru>
2024-03-22 23:57:36 +0300

    TWR: shareNetwork

--- LIB.Share/shareNetwork.h
+++ LIB.Share/shareNetwork.h
@@ -11,16 +11,19 @@ namespace share_network_udp
 {
 
 namespace asio_ip = boost::asio::ip;
+
+using tEndpoint = asio_ip::udp::endpoint;
+
 constexpr std::size_t PacketSizeMax = 1024;
 
-class UDP_Server
+class tUDPServerAsync
 {
 	asio_ip::udp::socket m_Socket;
 	asio_ip::udp::endpoint m_EndpointRemote;
 	std::array<char, PacketSizeMax> m_ReceiveBuffer;
 
 public:
-	UDP_Server(boost::asio::io_context& io_context, std::uint16_t port)
+	tUDPServerAsync(boost::asio::io_context& io_context, std::uint16_t port)
 		: m_Socket(io_context, asio_ip::udp::endpoint(asio_ip::udp::v4(), port))
 	{
 		StartReceive();
@@ -31,32 +34,35 @@ private:
 	{
 		m_Socket.async_receive_from(
 			boost::asio::buffer(m_ReceiveBuffer), m_EndpointRemote,
-			boost::bind(&UDP_Server::OnReceived, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+			boost::bind(&tUDPServerAsync::OnReceivedAsync, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
 	}
 
-	virtual utils::tVectorUInt8 OnReceived(const utils::tVectorUInt8& data) = 0;
+	virtual void OnReceived(const tEndpoint& endpoint, const utils::tVectorUInt8& data) = 0;
 	virtual void OnSent(boost::shared_ptr<utils::tVectorUInt8> packet, const boost::system::error_code& error, std::size_t bytes_transferred) = 0;
 
-	void OnReceived(const boost::system::error_code& error, std::size_t recvSize)
+	void OnReceivedAsync(const boost::system::error_code& error, std::size_t recvSize)
 	{
 		if (!error && recvSize)
-		{
-			boost::shared_ptr<utils::tVectorUInt8> Packet(new utils::tVectorUInt8(OnReceived({ m_ReceiveBuffer.begin(), m_ReceiveBuffer.begin() + recvSize })));
-			Send(Packet);
-		}
+			OnReceived(m_EndpointRemote, { m_ReceiveBuffer.begin(), m_ReceiveBuffer.begin() + recvSize });
 
 		StartReceive();
 	}
 
-public:
 	void Send(boost::shared_ptr<utils::tVectorUInt8> packet)
 	{
-		if (packet->empty())
+		if (!packet || packet->empty())
 			return;
 
 		m_Socket.async_send_to(
 			boost::asio::buffer(*packet), m_EndpointRemote,
-			boost::bind(&UDP_Server::OnSent, this, packet, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+			boost::bind(&tUDPServerAsync::OnSent, this, packet, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+	}
+
+public:
+	void Send(const tEndpoint& endpoint, const utils::tVectorUInt8& packet)
+	{
+		boost::shared_ptr<utils::tVectorUInt8> Packet(new utils::tVectorUInt8(packet));
+		Send(Packet);
 	}
 };
 
