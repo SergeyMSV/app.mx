e766aa8435a00b00d7c7bc1b3abdfd93c7ef7bde
Sergei <maslennikovserge@yandex.ru>
2025-09-10 00:46:02 +0300

    WebState: CPU thermo

--- WebState/server_cpu.js
+++ WebState/server_cpu.js
@@ -6,52 +6,62 @@ const sc_status = require('./status.js');
 
 exports.GetPage = function () {
     return '<tr><td></td><td>Load avg.:</td><td id="cpu_loadavg"></td></tr>' +
-        '<tr><td id="cpu_thermal_status" bgcolor="white"></td><td>CPU thermal:</td><td id="cpu_thermal"></td></tr>';
+        '<tr><td id="cpu_thermal_color" bgcolor="white"></td><td>CPU thermal:</td><td id="cpu_thermal_text"></td></tr>';
 }
 
 exports.GetPageData = function (a_config) {
     let data = {};
     data.cpu_loadavg = GetText(a_config.loadavg.path);
-    const thermal = (GetThermal(a_config.cpu_thermal.path) / a_config.cpu_thermaldiv).toFixed(2);
-    data.cpu_thermal = thermal + ' \xB0C'; // 0xB0 - celsius degree sign
-    data.cpu_thermal_status = GetThermalStatus(thermal);
+    data.cpu_thermal = GetThermal(a_config.cpu_thermal.path, a_config.cpu_thermaldiv);
     return data;
 }
 
-function GetThermal(a_fileName) {
+function GetThermal(a_filePath, a_thermalDiv) {
     try {
-        return np_fs.readFileSync(a_fileName, 'utf-8');
+        const tempStr = np_fs.readFileSync(a_filePath, 'utf-8');
+        if (isNaN(parseInt(tempStr)))
+            return sc_status.CPU_Thermal.NoData;
+        const temp = (parseInt(tempStr) / a_thermalDiv).toFixed(2);
+        return GetThermalStatus(temp);
     }
-    catch (err) {
-        console.error(err);
+    catch {
+        return sc_status.CPU_Thermal.NotSupported;
     }
-    return 0;
 }
 
-let g_cpu_thermal_status = sc_status.CPU.Color.None
+let g_cpu_thermal_status = sc_status.CPU_Thermal.None
 function GetThermalStatus(a_value) {
-    a_value = Math.floor(a_value);
+
     function CheckTemp(val, min, max) {
-        const hysteresis = g_cpu_thermal_status != sc_status.CPU.Color.None ? 1 : 0;
+        const hysteresis = g_cpu_thermal_status != sc_status.CPU_Thermal.None ? 1 : 0;
         return val >= min + hysteresis && val <= max - hysteresis; // hysteresis
     }
-    function CheckColor(val, min, max, color) {
+
+    function TryStatus(val, min, max, status) {
         if (!CheckTemp(val, min, max))
             return false;
-        g_cpu_thermal_status = color;
+        g_cpu_thermal_status = status;
         return true;
     }
-    if (CheckColor(a_value, 0, 30, sc_status.CPU.Color.Cold))
-        return g_cpu_thermal_status;
-    if (CheckColor(a_value, 30, 40, sc_status.CPU.Color.Normal))
-        return g_cpu_thermal_status;
-    if (CheckColor(a_value, 40, 50, sc_status.CPU.Color.Warm))
-        return g_cpu_thermal_status;
-    if (CheckColor(a_value, 50, 70, sc_status.CPU.Color.Hot))
-        return g_cpu_thermal_status;
-    if (CheckColor(a_value, 70, 100, sc_status.CPU.Color.Critical))
-        return g_cpu_thermal_status;
-    return g_cpu_thermal_status;
+
+    const stats = [
+        { status: sc_status.CPU_Thermal.Cold, t_min: -1000, t_max: 30 },
+        { status: sc_status.CPU_Thermal.Normal, t_min: 30, t_max: 40 },
+        { status: sc_status.CPU_Thermal.Warm, t_min: 40, t_max: 50 },
+        { status: sc_status.CPU_Thermal.Hot, t_min: 50, t_max: 70 },
+        { status: sc_status.CPU_Thermal.Critical, t_min: 70, t_max: 1000 },
+    ];
+
+    const temp = Math.floor(a_value);
+
+    for (const i of stats) {
+        if (TryStatus(temp, i.t_min, i.t_max, i.status))
+            break;
+    }
+
+    let st = Object.assign({}, g_cpu_thermal_status);
+    st.text = a_value + ' \xB0C'; // 0xB0 - celsius degree sign
+    return st;
 }
 
 function GetText(a_fileName) {
--- WebState/status.js
+++ WebState/status.js
@@ -1,8 +1,21 @@
 'use strict';
 
-exports.CPU = {
-    Color: {
-        None: 'gray',
-        Cold: 'blue', Normal: 'green', Warm: 'yellow', Hot: 'orange', Critical: 'red' // Temperature
-    }
+// Color priority:
+// gray             - not supported / not available
+// blue             - not good as green but still OK
+// green            - OK
+// yellow           - worse
+// orange           - more worse
+// red              - the worst condition
+
+exports.CPU_Thermal = {
+    None: { text: '', color: 'gray' },
+    NotSupported: { text: 'not supported', color: 'gray' },
+    NoData: { text: 'no data', color: 'gray' },
+    Error: { text: 'error', color: 'gray' },
+    Cold: { text: '', color: 'blue' },
+    Normal: { text: '', color: 'green' },
+    Warm: { text: '', color: 'yellow' },
+    Hot: { text: '', color: 'orange' },
+    Critical: { text: '', color: 'red' },
 }
