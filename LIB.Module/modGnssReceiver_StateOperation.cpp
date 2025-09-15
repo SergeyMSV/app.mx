f3f35e8ff212e0a4c8ab6f3aa500e878adf1b2d0
Sergei <maslennikovserge@yandex.ru>
2025-09-15 23:42:09 +0300

    mxgnss: modGnssRec

--- LIB.Module/modGnssReceiver_StateOperation.cpp
+++ LIB.Module/modGnssReceiver_StateOperation.cpp
@@ -41,8 +41,9 @@ bool tGnssReceiver::tStateOperation::Go()
 		return true;
 	}
 
-	auto Time_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(tClock::now() - m_StartTime).count();//C++11
-	double Time_us = static_cast<double>(Time_ns) / 1000;
+	auto Time_us = std::chrono::duration_cast<std::chrono::microseconds>(tClock::now() - m_StartTime).count();//C++11
+	//auto Time_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(tClock::now() - m_StartTime).count();//C++11
+	//double Time_us = static_cast<double>(Time_ns) / 1000;
 	if (Time_us > m_SettingsNMEA.PeriodMax)
 	{
 		ChangeState(new tStateError(m_pObj, "operation: no data within PeriodMAX"));
