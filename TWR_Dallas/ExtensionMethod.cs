29b545f35be0999a8d6e16df1800296cd049b4dd
Sergei <maslennikovserge@yandex.ru>
2026-01-08 00:56:41 +0300

    TWR: TWR_Dallas

---
+++ TWR_Dallas/ExtensionMethod.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ExtensionMethods
+{
+    public static class StringExtension
+    {
+        public static bool IsKeyGroup(this string str)
+        {
+            var KeyIDRaw = str.Where(char.IsLetterOrDigit).ToArray()!;
+            return KeyIDRaw?.Length > 0;
+        }
+
+        public static bool IsKeyLabel(this string str)
+        {
+            var KeyIDRaw = str.Where(char.IsLetterOrDigit).ToArray()!;
+            return KeyIDRaw?.Length > 0;
+        }
+
+        public static bool IsKeyID(this string str)
+        {
+            var KeyIDRaw = str.Where((char a) => { return char.IsDigit(a) || (a >= 'a' && a <= 'f') || (a >= 'A' && a <= 'F'); }).ToArray()!;
+            return KeyIDRaw?.Length == 12;
+        }
+
+        public static bool IsKeyFieldChanged(this string str, string value)
+        {
+            return str.Length == 0 || str != value;
+        }
+    }
+}
