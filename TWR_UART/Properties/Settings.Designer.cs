a6541f4fbdadcfd9864cd1a95c043a1d56c3a523
Sergei <maslennikovserge@yandex.ru>
2026-03-22 00:09:02 +0300

    TWR_UART: utils_mx

--- TWR_UART/Properties/Settings.Designer.cs
+++ TWR_UART/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TWR_UART.Properties {
+namespace SergeM.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
