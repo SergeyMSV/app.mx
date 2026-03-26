6f809bcaf50fd4c9495dbd090a3e53e861561edc
Sergei <maslennikovserge@yandex.ru>
2026-03-26 22:26:24 +0300

    TWR: TWR_UART

---
+++ TWR_UART/utilsProtocolNMEA.cs
@@ -0,0 +1,19 @@
+﻿// utilsProtocolNMEA: 2026-01-09
+using System.Reflection;
+using System.Text;
+
+namespace utils
+{
+    public static class ProtocolNMEA
+    {
+        public static string GetCRC(string data)
+        {
+            byte checksum = 0;
+            for (int i = 0; i < data.Length; ++i)
+            {
+                checksum ^= (byte)data[i];
+            }
+            return checksum.ToString("X2");
+        }
+    }
+}
