63a3b3856b6badd4cc02f47299ce9948e907cb12
Sergei <maslennikovserge@yandex.ru>
2026-03-27 23:49:19 +0300

    GNSS_Simulator

---
+++ GNSS_Simulator/FormMain_Utils.cs
@@ -0,0 +1,100 @@
+﻿// In *.csproj.user the following statement:
+// 
+//     <Compile Update="FormMain_Utils.cs">
+//       <SubType>Form</SubType>
+//     </Compile>
+// 
+// shall be replaces with the next one:
+// 
+//     <Compile Update="FormMain_Utils.cs">
+//       <DependentUpon>FormMain.cs</DependentUpon>
+//       <SubType>Form</SubType>
+//     </Compile>
+
+namespace SergeM
+{
+    public partial class FormMain
+    {
+        void InitializeFormSizes()
+        {
+            Size FormSize = Properties.Settings.Default.FormMainSize;
+            if (FormSize.Height < this.MinimumSize.Height || FormSize.Width < this.MinimumSize.Width)
+                FormSize = this.MinimumSize;
+            if (FormSize.Height > SystemInformation.VirtualScreen.Height || FormSize.Width > SystemInformation.VirtualScreen.Width)
+                FormSize = this.MinimumSize;
+            if (IsLocationOnScreen(Properties.Settings.Default.FormMainPosition))
+            {
+                this.Location = Properties.Settings.Default.FormMainPosition;
+            }
+            else
+            {
+                this.StartPosition = FormStartPosition.CenterScreen;
+            }
+            this.Size = FormSize;
+        }
+
+        void SaveFormSizes()
+        {
+            if (this.WindowState != FormWindowState.Normal)
+                return;
+            if (Properties.Settings.Default.FormMainPosition == this.Location &&
+                Properties.Settings.Default.FormMainSize == this.Size)
+                return;
+            Properties.Settings.Default.FormMainPosition = this.Location;
+            Properties.Settings.Default.FormMainSize = this.Size;
+            Properties.Settings.Default.Save();
+        }
+
+        bool IsLocationOnScreen(Point location)
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                Rectangle screenBounds = screen.Bounds;
+                Rectangle formBounds = new Rectangle(location, Application.OpenForms.Count > 0 ? Application.OpenForms[0]!.Size : new Size(800, 600));
+                if (screenBounds.Contains(formBounds))
+                    return true;
+            }
+            return false;
+        }
+
+        #region radioButton
+
+        enum SendProtocol : byte
+        {
+            None,
+            Line,
+            NMEA,
+            NMEA_No_CRC,
+        }
+
+        void InitializeSendRadioButtons()
+        {
+            switch ((SendProtocol)Properties.Settings.Default.SendProtocolID)
+            {
+                case SendProtocol.None: radioButtonSendAsIs.Checked = true; break;
+                case SendProtocol.Line: radioButtonSendLine.Checked = true; break;
+                case SendProtocol.NMEA: radioButtonSendNMEA.Checked = true; break;
+                case SendProtocol.NMEA_No_CRC: radioButtonSendNMEANoCRC.Checked = true; break;
+            }
+        }
+
+        void SaveSendRadioButtons()
+        {
+            SendProtocol CurrentSendProtocol = SendProtocol.None;
+            if (radioButtonSendLine.Checked)
+                CurrentSendProtocol = SendProtocol.Line;
+            if (radioButtonSendNMEA.Checked)
+                CurrentSendProtocol = SendProtocol.NMEA;
+            if (radioButtonSendNMEANoCRC.Checked)
+                CurrentSendProtocol = SendProtocol.NMEA_No_CRC;
+
+            if (Properties.Settings.Default.SendProtocolID != (byte)CurrentSendProtocol)
+            {
+                Properties.Settings.Default.SendProtocolID = (byte)CurrentSendProtocol;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        #endregion
+}
+}
---
+++ GNSS_Simulator/ReceiverPolicyMTK_SC872-A.cs
@@ -0,0 +1,30 @@
+﻿namespace SergeM
+{
+    internal class ReceiverPolicyMTK_SC872_A : IReceiverPolicy
+    {
+        //public event EventHandler<NaviEventArgs>? NaviChanged;
+        public event EventHandler<PortBaudrateEventArgs>? PortBaudrateChanged;
+
+        public List<string> MakeOutMsgSetRestart(int baudrate)
+        {
+            List<string> Strs = new();
+            Strs.Add("SC872-A\r\n");
+            Strs.Add("RESTART\r\n");
+            Strs.Add("it has not been implemented yet.\r\n");
+            return Strs;
+        }
+
+        public List<string> MakeOutMsgSetNavi()
+        {
+            List<string> Strs = new();
+            Strs.Add("SC872-A\r\n");
+            Strs.Add("it has not been implemented yet.\r\n");
+            return Strs;
+        }
+
+        public List<string> HandleMsg(string msg, int baudrate)
+        {
+            return new();
+        }
+    }
+}
